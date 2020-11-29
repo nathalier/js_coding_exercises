47c74a4304dcd666ec78ac8be5add827f282dd9a
nathalier <nathalier@gmail.com>
2020-11-29 18:04:40 +0000

    Complete exercise002

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,31 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city === "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people / 40)
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  sheepsNum = 0
+  for(const animal of arr)
+    if (animal == "sheep")
+      sheepsNum++;
+  return sheepsNum
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.address && (person.address.city == "Manchester") && 
+          person.address.postCode && (person.address.postCode[0] == "M")
 }
 
 module.exports = {
