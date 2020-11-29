6712a84348d1c2b5c129dbd0809c63bf380f6194
nathalier <nathalier@gmail.com>
2020-11-29 20:51:26 +0000

    Complete exercise003

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,28 +1,34 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map(x => x ** 2)
 }
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  return words.reduce((a, b) => a + b.substring(0, 1).toUpperCase() + b.substring(1, b.length))
 }
 
 function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  var count = 0
+  for (const p of people)
+    count += p.subjects.length
+  return count
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  for (const item of menu)
+    if (item.ingredients.includes(ingredient))
+      return true;
+  return false;
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+  return [...new Set(arr1.filter(value => arr2.includes(value)))].sort()
 }
 
 module.exports = {
